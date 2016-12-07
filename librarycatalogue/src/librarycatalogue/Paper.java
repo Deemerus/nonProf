bfa948c234fc715035e1ae22d301bf4c62d043e0
Deemerus <balcersz@gmail.com>
2016-12-07 16:37:47 +0100

    Started library catalogue project
    implemented paper class and Book, Newspaper and Letter subclasses with
    constructors. Implemented add method which creates new object.

---
+++ librarycatalogue/src/librarycatalogue/Paper.java
@@ -0,0 +1,8 @@
+package librarycatalogue;
+
+public class Paper {
+	protected int id;
+	protected String title = null;
+	protected int date;
+	protected static int count=0;
+}
--- librarycatalogue/src/librarycatalogue/app.java
+++ librarycatalogue/src/librarycatalogue/app.java
@@ -1,14 +1,27 @@
 package librarycatalogue;
 
+import java.util.Scanner;
+
 public class app {
 
-	public app() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
-
+	Paper add(){
+		Scanner input = new Scanner(System.in);
+		String type = input.next();
+		Paper paper = null;
+		if(type.equals("book"))
+		{
+			paper = new Book();
+		}else if(type.equals("newspaper")){
+			paper = new Newspaper();
+		}else if(type.equals("letter")){
+			paper=new Letter();
+		}else{
+			System.out.println("Wrong type of paper.");
+		}
+		input.close();
+		return paper;
+	}
 }
