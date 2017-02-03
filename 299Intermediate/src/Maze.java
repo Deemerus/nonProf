3d788fcc7325e25216834e919d3231d67fd487a8
Deemerus <balcersz@gmail.com>
2017-02-03 12:51:54 +0100

    Finished 299 intermediate challenge

--- 299Intermediate/src/Maze.java
+++ 299Intermediate/src/Maze.java
@@ -1,11 +1,14 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Maze {
 	private int[][] maze;
 	private int length;
 	private int height;
+	List<Node> nodes = new ArrayList<Node>();
 
 	Maze() {
 		try (Scanner in = new Scanner(new File("maze.bin"))) {
@@ -52,6 +55,50 @@ public class Maze {
 			}
 		} else {
 			return false;
+		}
+	}
+
+	public void setMaze(int height, int length, int value) {
+		this.maze[height][length] = value;
+	}
+
+	public void addNode(Node node) {
+		nodes.add(node);
+	}
+
+	public void placeNodesOnMaze() {
+		for (Node node : nodes) {
+			this.setMaze(node.getHeight(), node.getLength(), -1);
+		}
+	}
+
+	public void lookForNeighbours() {
+		for (Node node : nodes) {
+			searchNeighbour(node, node.getHeight(), node.getLength(), 1, 0, 1);
+			searchNeighbour(node, node.getHeight(), node.getLength(), -1, 0, 1);
+			searchNeighbour(node, node.getHeight(), node.getLength(), 0, 1, 1);
+			searchNeighbour(node, node.getHeight(), node.getLength(), 0, -1, 1);
+		}
+	}
+
+	public void searchNeighbour(Node node, int height, int length, int heightChange, int lengthChange, int distance) {
+		if (maze[height + heightChange][length + lengthChange] == -1) {
+			node.addNeighbour(height + heightChange, length + lengthChange, distance);
+		} else if (maze[height + heightChange][length + lengthChange] == 1) {
+			if (heightChange == 0) {
+				searchNeighbour(node, height + heightChange, length + lengthChange, 1, 0, distance + 1);
+				searchNeighbour(node, height + heightChange, length + lengthChange, -1, 0, distance + 1);
+				searchNeighbour(node, height + heightChange, length + lengthChange, 0, lengthChange, distance + 1);
+			} else if (lengthChange == 0) {
+				searchNeighbour(node, height + heightChange, length + lengthChange, heightChange, 0, distance + 1);
+				searchNeighbour(node, height + heightChange, length + lengthChange, 0, 1, distance + 1);
+				searchNeighbour(node, height + heightChange, length + lengthChange, 0, -1, distance + 1);
+			}
+		}
+	}
+	public void printNodesNeighbours(){
+		for(Node node:nodes){
+			node.printNeighbours();
 		}
 	}
 }
