92c43a84a5c946aff11db389bbde9cd7d26002f5
Deemerus <balcersz@gmail.com>
2017-02-14 15:53:40 +0100

    Added 302Easy + bonus

--- 301EasyIntermediate/src/App.java
+++ 301EasyIntermediate/src/App.java
@@ -7,37 +7,38 @@ import java.util.Scanner;
 public class App {
 
 	public static void main(String[] args) {
-		try(Scanner scanner = new Scanner(System.in)){
+		try (Scanner scanner = new Scanner(System.in)) {
 			String pattern = getPattern(scanner.nextLine());
-			try(Scanner scanner2 = new Scanner(new File("enable1.txt"))){
-				while(scanner2.hasNextLine()){
+			try (Scanner scanner2 = new Scanner(new File("enable1.txt"))) {
+				while (scanner2.hasNextLine()) {
 					String nextLine = scanner2.nextLine();
-					if(nextLine.matches(pattern)){
+					if (nextLine.matches(pattern)) {
 						System.out.println(nextLine);
 					}
 				}
-			}catch (FileNotFoundException e) {
+			} catch (FileNotFoundException e) {
 				System.out.println("File enable1.txt not found.");
 			}
 		}
 	}
-	public static String getPattern(String toPattern){
+
+	public static String getPattern(String toPattern) {
 		String pattern = new String();
 		Boolean check = false;
 		List<Character> chars = new LinkedList<Character>();
-		for(int i=0;i<toPattern.length();i++){
-			for(int j=0;j<chars.size();j++){
-				if(toPattern.charAt(i)==chars.get(j)){
-					pattern+="\\" + (j+2);
-					check=true;
+		for (int i = 0; i < toPattern.length(); i++) {
+			for (int j = 0; j < chars.size(); j++) {
+				if (toPattern.charAt(i) == chars.get(j)) {
+					pattern += "\\" + (j + 2);
+					check = true;
 				}
 			}
-			if(!check){
-				pattern+="(.)";
+			if (!check) {
+				pattern += "(.)";
 				chars.add(toPattern.charAt(i));
 			}
-			check=false;
+			check = false;
 		}
-		return "(.*)"+pattern+"(.*)";
+		return "(.*)" + pattern + "(.*)";
 	}
 }
---
+++ 302Easy/src/Element.java
@@ -0,0 +1,15 @@
+public class Element {
+	private String name;
+	private double weight;
+	public String getName() {
+		return name;
+	}
+	public double getWeight() {
+		return weight;
+	}
+	public Element(String name, double weight){
+		this.name=name;
+		this.weight=weight;
+	}
+	
+}
